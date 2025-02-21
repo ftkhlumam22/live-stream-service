f13e07f3f661c67deb9c1d5c123fc3a9ff9bfe2c
Fatkhul Umam <88369914+ftkhlumam22@users.noreply.github.com>
2025-02-21 10:45:37 +0700

    add path api on route

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(compression({ level: 8 }));
 // Cors Origin
 app.use(cors());
 
-app.use("/", routesApi);
+app.use("/api", routesApi);
 
 app.use((_, res) => {
   return httpResponse.notFoundResponse(res, "API not found");
